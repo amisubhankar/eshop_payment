d289f595dc64000c1b2f21ff88858edbf62e867f
amisubhankar <subhankar.manna99@gmail.com>
2024-06-24 20:35:23 +0530

    First commit

---
+++ src/main/java/com/eshop/payment/controller/PaymentController.java
@@ -0,0 +1,15 @@
+package com.eshop.payment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @GetMapping("/create")
+    public String test(){
+        return "Payment working !!";
+    }
+}
